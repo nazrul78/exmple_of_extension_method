5b37a9d612c5a7048dfa48c0bbc3ecc3bb467781
Nazrul Islam <nnakib78@gmail.com>
2022-12-18 01:09:11 +0600

    Add getter & setter method.

--- lib/src/controllers/example_controller.dart
+++ lib/src/controllers/example_controller.dart
@@ -19,11 +19,42 @@ class ExampleController extends GetxController {
 
   final fruitList = RxList<String>([]);
 
-  //=============== Ezample of Seter & geter ====================
+  //=============== Example of Abstract class ====================
 
   final userList = RxList<String>([]);
   final userInput = RxString('');
 
   final productList = RxList<String>([]);
   final inputProduct = RxString('');
+
+  //=============== Example of Seter & geter ====================
+
+  final empName = RxString('');
+  final empId = RxString('');
+  final empAge = RxInt(0);
+  final empSalary = RxInt(0);
+
+  // String get employeeName {
+  //   return empName.value;
+  // }
+
+// =========== Example of Getter method ============
+  String get getEmployeeName => empName.value;
+  String get getEmployeeId => empId.value;
+  int get getEmployeeAge => empAge.value;
+  int get getEmployeeSalary => empSalary.value;
+
+// =========== Example of Setter method ============
+  set setEmployeName(String name) => empName.value = name;
+  set setEmployeId(String id) => empId.value = id;
+  set setEmployeAge(int age) => empAge.value = age;
+  set setEmployeSalary(int salary) => empSalary.value = salary;
+
+  //   set employeeAge(int age) {
+  //     if(age<= 18) {
+  //        print("Employee Age should be greater than 18 Years.");
+  //     } else {
+  //        this.empAge = age;
+  //     }
+  // }
 }
